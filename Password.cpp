026f5e82a5f5d0cfef3442a00512380cec86ad40
lizetGutierrez <lizet.gutierrez12@gmail.com>
2024-11-13 11:54:02 -0800

    changed to fix bugs

--- Password.cpp
+++ Password.cpp
@@ -1,5 +1,6 @@
 #include "Password.h"
 #include <string>
+#include <cctype>  
 
 using std::string;
 
@@ -24,6 +25,15 @@ int Password::count_leading_characters(string phrase){
   receives a string and returns whether it has both at least one upper-case
   letter and at least one lower-case letter
 */
-bool Password::has_mixed_case(string pass){
+bool Password::has_mixed_case(string pass) {
+  bool has_upper = false;
+  bool has_lower = false;
+
+  for (char ch : pass) {
+    if (isupper(ch)) has_upper = true;
+    if (islower(ch)) has_lower = true;
+    if (has_upper && has_lower) return true;
+  }
+
   return false;
 }
--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -5,11 +5,11 @@
 #include <gtest/gtest.h>
 #include "Password.h"
 
-class PracticeTest : public ::testing::Test
+class PasswordTest : public ::testing::Test
 {
 	protected:
-		PracticeTest(){} //constructor runs before each test
-		virtual ~PracticeTest(){} //destructor cleans up after tests
+		PasswordTest(){} //constructor runs before each test
+		virtual ~PasswordTest(){} //destructor cleans up after tests
 		virtual void SetUp(){} //sets up before each test (after constructor)
 		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
@@ -19,4 +19,64 @@ TEST(PasswordTest, single_letter_password)
 	Password my_password;
 	int actual = my_password.count_leading_characters("Z");
 	ASSERT_EQ(1, actual);
+	
+}
+
+
+
+//practice quiz
+TEST(PasswordTest, mixed_case_with_upper_and_lower)
+{
+    Password my_password;
+    bool actual = my_password.has_mixed_case("Hello");
+    ASSERT_TRUE(actual);
+}
+
+TEST(PasswordTest, all_lower_case)
+{
+    Password my_password;
+    bool actual = my_password.has_mixed_case("hello");
+    ASSERT_FALSE(actual);
+}
+
+TEST(PasswordTest, all_upper_case)
+{
+    Password my_password;
+    bool actual = my_password.has_mixed_case("HELLO");
+    ASSERT_FALSE(actual);
+}
+
+TEST(PasswordTest, mixed_case_with_symbols)
+{
+    Password my_password;
+    bool actual = my_password.has_mixed_case("HeLLo@123");
+    ASSERT_TRUE(actual);
+}
+
+TEST(PasswordTest, single_upper_case_letter)
+{
+    Password my_password;
+    bool actual = my_password.has_mixed_case("A");
+    ASSERT_FALSE(actual);
+}
+
+TEST(PasswordTest, single_lower_case_letter)
+{
+    Password my_password;
+    bool actual = my_password.has_mixed_case("a");
+    ASSERT_FALSE(actual);
+}
+
+TEST(PasswordTest, empty_string)
+{
+    Password my_password;
+    bool actual = my_password.has_mixed_case("");
+    ASSERT_FALSE(actual);
+}
+
+TEST(PasswordTest, mixed_case_multiple_letters)
+{
+    Password my_password;
+    bool actual = my_password.has_mixed_case("aBcDeFgH");
+    ASSERT_TRUE(actual);
 }
